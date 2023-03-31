4d8713b08a2508526cf4d9ec87996e4ff9670758
Dominik Borek <xborek12@stud.fit.vut.cz>
2023-03-31 12:51:14 +0200

    upraveni ui aplikace

--- src/ui/kalkulacka/mainwindow.cpp
+++ src/ui/kalkulacka/mainwindow.cpp
@@ -6,10 +6,14 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color: #3c6e71;");
+    setWindowFlags(Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
+    ui->Rest_of_number->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-style: outset; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
+    ui->Display->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-style: outset; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
+    ui->Display->setReadOnly(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
--- src/ui/kalkulacka/mainwindow.h
+++ src/ui/kalkulacka/mainwindow.h
@@ -15,6 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_Display_textEdited(const QString &arg1);
+
+    void on_Display_inputRejected();
+
 private:
     Ui::MainWindow *ui;
 };
