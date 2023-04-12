36024aaf24eba7e35202ee593a027ed6fed9cdb8
Dominik Borek <xborek12@stud.fit.vut.cz>
2023-04-12 13:18:57 +0200

    opraveni chyby hlavicky qt

--- src/ui/kalkulacka - kopie - 08.04.2023/mainwindow.cpp
+++ src/ui/kalkulacka - kopie - 08.04.2023/mainwindow.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     this->setStyleSheet("background-color: #3c6e71;");
@@ -11,6 +12,12 @@ MainWindow::MainWindow(QWidget *parent)
     ui->Rest_of_number->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
     ui->Display->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-style: outset; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
 
+
+    //QRegExp rx("-?\\d{1,3}");
+    //QValidator *validator = new QRegExpValidator(rx, this);
+
+    //QLineEdit *edit = new QLineEdit(this);
+    //edit->setValidator(validator);
 }
 
 MainWindow::~MainWindow()
