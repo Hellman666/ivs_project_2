ccfd93f2008d5e69156257eb7849a13a685983e9
Dominik Borek <xborek12@stud.fit.vut.cz>
2023-04-12 15:24:24 +0200

    vytvoření icon.ico a icon.png

--- src/ui/kalkulacka/mainwindow.cpp
+++ src/ui/kalkulacka/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,10 +11,14 @@ MainWindow::MainWindow(QWidget *parent)
     setWindowFlags(Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
     ui->Rest_of_number->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
     ui->Display->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-style: outset; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
-
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+void MainWindow::on_Number0_clicked()
+{
+    qDebug() <<"0";
+}
+
--- src/ui/kalkulacka/mainwindow.h
+++ src/ui/kalkulacka/mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_Number0_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
