a4429713887203b4f71f80f29b197a861c6ce46b
Dominik <dominikborek@seznam.cz>
2023-04-13 10:53:37 +0200

    uprava funkce tlacitek a slozkovani

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -17,8 +17,148 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 void MainWindow::on_Number0_clicked()
 {
     qDebug() <<"0";
+}
+
+
+void MainWindow::on_Number1_clicked()
+{
+    qDebug() <<"1";
+}
+
+
+
+void MainWindow::on_Number2_clicked()
+{
+    qDebug() <<"2";
+}
+
+
+void MainWindow::on_Number3_clicked()
+{
+    qDebug() <<"3";
+}
+
+
+void MainWindow::on_Number4_clicked()
+{
+    qDebug() <<"4";
+}
+
+
+void MainWindow::on_Number5_clicked()
+{
+    qDebug() <<"5";
+}
+
+
+void MainWindow::on_Number6_clicked()
+{
+    qDebug() <<"6";
+}
+
+
+void MainWindow::on_Number7_clicked()
+{
+    qDebug() <<"7";
+}
+
+
+void MainWindow::on_Number8_clicked()
+{
+    qDebug() <<"8";
+}
+
+
+void MainWindow::on_Number9_clicked()
+{
+    qDebug() <<"9";
+}
+
+
+void MainWindow::on_Dot_clicked()
+{
+    qDebug() <<".";
+}
+
+
+void MainWindow::on_Equals_clicked()
+{
+    qDebug() <<"=";
+}
+
+
+void MainWindow::on_Div_clicked()
+{
+    qDebug() <<"/";
+}
+
+
+void MainWindow::on_Mul_clicked()
+{
+    qDebug() <<"x";
+}
+
+
+void MainWindow::on_Minus_clicked()
+{
+    qDebug() <<"-";
+}
+
+
+void MainWindow::on_Plus_clicked()
+{
+    qDebug() <<"+";
+}
+
+
+void MainWindow::on_Root_clicked()
+{
+    qDebug() <<"√";
+}
+
+
+void MainWindow::on_Power_clicked()
+{
+    qDebug() <<"^";
+}
+
+
+void MainWindow::on_Right_bracket_clicked()
+{
+    qDebug() <<")";
+}
+
+
+void MainWindow::on_Left_bracket_clicked()
+{
+    qDebug() <<"(";
+}
+
+
+void MainWindow::on_Idiv_clicked()
+{
+    qDebug() <<"//";
+}
+
+
+void MainWindow::on_Fact_clicked()
+{
+    qDebug() <<"!";
+}
+
+
+void MainWindow::on_Clear_clicked()
+{
+    qDebug() <<"C";
+}
+
+
+void MainWindow::on_Backspace_clicked()
+{
+    qDebug() <<"<-";
 }
 
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -17,6 +17,29 @@ public:
 
 private slots:
     void on_Number0_clicked();
+    void on_Number1_clicked();
+    void on_Number9_clicked();
+    void on_Number2_clicked();
+    void on_Number3_clicked();
+    void on_Number4_clicked();
+    void on_Number5_clicked();
+    void on_Number6_clicked();
+    void on_Number7_clicked();
+    void on_Number8_clicked();
+    void on_Dot_clicked();
+    void on_Equals_clicked();
+    void on_Div_clicked();
+    void on_Mul_clicked();
+    void on_Minus_clicked();
+    void on_Plus_clicked();
+    void on_Root_clicked();
+    void on_Power_clicked();
+    void on_Right_bracket_clicked();
+    void on_Left_bracket_clicked();
+    void on_Idiv_clicked();
+    void on_Fact_clicked();
+    void on_Clear_clicked();
+    void on_Backspace_clicked();
 
 private:
     Ui::MainWindow *ui;
