6279e7a134cd3c87c67aab29d6424416b8d78586
Dominik <dominikborek@seznam.cz>
2023-04-13 12:10:08 +0200

    vypisovani cisel tlacitka na display

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "./ui_mainwindow.h"
 #include <QDebug>
 
+
+double calcVal = 0.0;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,11 +14,61 @@ MainWindow::MainWindow(QWidget *parent)
     setWindowFlags(Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
     ui->Rest_of_number->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
     ui->Display->setStyleSheet("QLineEdit {color: white; background-color: #284b63; padding: 10px; color: white; border-style: outset; border-width: 2px; border-radius: 15px; border-color: #b8cbd8; }");
+
+    // Put 0.0 in Display
+    ui->Display->setText(QString::number(calcVal));
+
+    // Will hold references to all the number buttons
+    QPushButton *numButtons[10];
+
+    // Cycle through locating the buttons
+    for(int i = 0; i < 10; ++i){
+        QString butName = "Button" + QString::number(i);
+
+        // Get the buttons by name and add to array
+        numButtons[i] = MainWindow::findChild<QPushButton *>(butName);
+
+        // When the button is released call num_pressed()
+        connect(numButtons[i], SIGNAL(released()), this,
+                SLOT(NumPressed()));
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::NumPressed(){
+
+    // Sender returns a pointer to the button pressed
+    QPushButton *button = (QPushButton *)sender();
+
+    // Get number on button
+    QString butVal = button->text();
+
+    // Get the value in the display
+    QString displayVal = ui->Display->text();
+
+    if((displayVal.toDouble() == 0) || (displayVal.toDouble() == 0.0)){
+
+        // calcVal = butVal.toDouble();
+        ui->Display->setText(butVal);
+
+    } else {
+        // Put the new number to the right of whats there
+        QString newVal = displayVal + butVal;
+
+        // Double version of number
+        double dblNewVal = newVal.toDouble();
+
+        // calcVal = newVal.toDouble();
+
+        // Set value in display and allow up to 16
+        // digits before using exponents
+        ui->Display->setText(QString::number(dblNewVal, 'g', 16));
+
+    }
 }
 
 void MainWindow::on_Number0_clicked()
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -16,6 +16,7 @@ public:
     ~MainWindow();
 
 private slots:
+    void NumPressed();
     void on_Number0_clicked();
     void on_Number1_clicked();
     void on_Number9_clicked();
