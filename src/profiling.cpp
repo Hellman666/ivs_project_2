e6be96d32ea139730d30efcd5c7518c6118a53b0
KeyiOs <77984692+KeyiOs@users.noreply.github.com>
2023-04-24 17:49:59 +0200

    Funkcni verze 1.0

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include "includes/math.h"
 
 double vypocet_prumeru(double arr[], double size) {
@@ -8,16 +8,16 @@ double vypocet_prumeru(double arr[], double size) {
 }
 
 double vypocet_smerodatne_odchylky(double arr[], double size) {
-    double prumer = vypocet_prumeru(arr, size);
+    double prumer = mul(size, power(vypocet_prumeru(arr, size), 2));
     double s = 0;
-    for (int i = 0; i < size; ++i) s = sum(s, power(sub(arr[i], prumer), 2));
-    return root(div(s, sub(size, 1)), 2);
+    for (int i = 0; i < size; ++i) s = sum(s, power(arr[i], 2));
+    return root(mul(sub(s, prumer), size-1), 2);
 }
 
 int main() {
     double numbers[] = {1, 2, 3, 4, 5};
     double size = div((double)sizeof(numbers), (double)sizeof(numbers[0]));
     double smerodatna_odchylka = vypocet_smerodatne_odchylky(numbers, size);
-    std::cout << "Výběrová směrodatná odchylka: " << smerodatna_odchylka << std::endl;
+    std::cout << "Odchylka: " << smerodatna_odchylka << std::endl;
     return 0;
 }
